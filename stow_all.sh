9b779fba1e754a99493cc3a6de6e7eebddb2beab
Jim Lloyd <jim@play-hearts.online>
2024-09-21 17:54:26 +0000

    stow_all.sh: force stowing over existing files

--- stow_all.sh
+++ stow_all.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-cd ~/.dotfiles
 
-for dir in */; do
-    stow "${dir%/}"
+# The location of the git repo.
+DOTFILES="${HOME}/dotfiles"
+
+# The target for the packages. For now, must be home dir
+TARGET="${HOME}"
+
+for pkg in git bash; do
+    for file in $(ls -1A "${DOTFILES}/${pkg}"); do
+        rm ${TARGET}/$file
+    done
+
+    stow --dir="${DOTFILES}" --target="${TARGET}" "${pkg}"
 done
