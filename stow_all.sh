e048ead8bbed57515013174153949906ebf49c2a
Jim Lloyd <jim@play-hearts.online>
2024-09-22 20:05:32 +0000

    stow_all.sh: add local, use rm -rf

--- stow_all.sh
+++ stow_all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# This script uses `stow`, but the author of `stow` would say it misuses `stow`.
+# `stow` never deletes a file that it isn't sure it is responsible for.
+# This script explicitly changes that policy, and instead assumes that
+# the script is being used to provision a new system and will need to replace
+# files that are installed in the base system.
+
+# The script should only be used in controlled cases like creating a Docker container.
 
 # The location of the git repo.
 DOTFILES="${HOME}/dotfiles"
@@ -7,9 +14,9 @@ DOTFILES="${HOME}/dotfiles"
 # The target for the packages. For now, must be home dir
 TARGET="${HOME}"
 
-for pkg in git bash; do
+for pkg in git bash local; do
     for file in $(ls -1A "${DOTFILES}/${pkg}"); do
-        rm ${TARGET}/$file
+        rm -rf ${TARGET}/$file
     done
 
     stow --dir="${DOTFILES}" --target="${TARGET}" "${pkg}"
